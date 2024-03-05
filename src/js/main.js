95363354c901b1c380d08ef6fbf7bd5a70ebce00
Mario Gutierrez <mgutierrez1@logitech.com>
2024-03-05 19:12:33 +0100

    Use webcam handler to detect landmarks on each new video frame

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
 import * as Hands from "@mediapipe/hands";
 import * as DrawingUtils from "@mediapipe/drawing_utils";
 import * as Vision from "@mediapipe/tasks-vision";
+import * as WebcamHandler from "./webcamHandler.js";
+
+const canvasElement = document.getElementById("output_canvas");
+const canvasCtx = canvasElement.getContext("2d");
+const video = document.getElementById("webcam");
+
+
+let runningMode = "VIDEO";
+let handLandmarker = undefined;
 
 const AddToDom = (text) => {
     let par = document.createElement("p");
@@ -19,11 +28,6 @@ const DrawCircle = (p, canvas, context, radius, lineWidth, fillColor, lineColor)
     context.stroke();
 }
 
-const canvasElement = document.getElementById("output_canvas");
-const canvasCtx = canvasElement.getContext("2d");
-
-let runningMode = "IMAGE";
-let handLandmarker = undefined;
 // Before we can use HandLandmarker class we must wait for it to finish
 // loading. Machine Learning models can be large and take a moment to
 // get everything needed to run.
@@ -39,30 +43,35 @@ const createHandLandmarker = async () => {
     });
     AddToDom("HandLandmakrer loaded!");
 };
-await createHandLandmarker();
-const image = document.getElementById("handImage");
-canvasElement.width = image.width;
-canvasElement.height = image.height;
 
-const results = handLandmarker.detect(image);
-canvasCtx.save();
-canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-if (results.landmarks) {
-    console.log(results);
-    for (const landmarks of results.landmarks) {
-        DrawingUtils.drawConnectors(canvasCtx, landmarks, Hands.HAND_CONNECTIONS, {
-            color: "#00FF00",
-            lineWidth: 2
-        });
-        DrawingUtils.drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: 2 });
-        // See hand-landmarks.png in mediapipe_info folder
-        // Ref: https://developers.google.com/mediapipe/solutions/vision/hand_landmarker
-        // Thumb tip position
-        DrawCircle(landmarks[4], canvasElement, canvasCtx, 8, 2, '#00aaff', '#ff00ff');
-        // Index finger tip position
-        DrawCircle(landmarks[8], canvasElement, canvasCtx, 8, 2, '#00ff00', '#00aaff');
+const detectionCallback = (results) => {
+    canvasElement.style.width = video.videoWidth;
+    canvasElement.style.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvasElement.height = video.videoHeight;
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    if (results.landmarks) {
+        //console.log(results);
+        for (const landmarks of results.landmarks) {
+            DrawingUtils.drawConnectors(canvasCtx, landmarks, Hands.HAND_CONNECTIONS, {
+                color: "#00FF00",
+                lineWidth: 2
+            });
+            DrawingUtils.drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: 0 });
+            // See hand-landmarks.png in mediapipe_info folder
+            // Ref: https://developers.google.com/mediapipe/solutions/vision/hand_landmarker
+            // Thumb tip position
+            DrawCircle(landmarks[4], canvasElement, canvasCtx, 8, 2, '#00aaff', '#ff00ff');
+            // Index finger tip position
+            DrawCircle(landmarks[8], canvasElement, canvasCtx, 8, 2, '#00ff00', '#00aaff');
 
+        }
     }
+    canvasCtx.restore();
 }
-canvasCtx.restore();
+
+await createHandLandmarker();
+WebcamHandler.InitWebcam(video, handLandmarker, detectionCallback);
+
 export { AddToDom };
